c9173d0a9927b037d5211dbbc8f134bd64a78e04
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2025-04-19 19:11:59 -0300

    inicia projeto

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="">
+      <h1> {{ data.results[0].name }} </h1>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { data, status, error, refresh, clear } = await useFetch("https://rickandmortyapi.com/api/character");
+
+console.log(data.value);
+</script>
