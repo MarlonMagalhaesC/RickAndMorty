c2c88b2a6d2d2edd6c1349ae4980a695cc734b93
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2025-04-20 16:10:22 -0300

    Cria componente card

--- app.vue
+++ app.vue
@@ -1,53 +1,19 @@
 <template>
-<div class="bg-black">
+<div class="bg-gray-900">
   <div class="flex flex-wrap gap-4 justify-center max-w-[1300px] mx-auto">
     <div
       v-for="item in data.results"
       :key="item.id"
       class="flex flex-col gap-2 w-[294px] h-[425px] bg-[#313234] text-white rounded-xl"
     >
-      <div class="mx-auto mt-4 border border-[#000000] rounded-2xl w-[262px] h-[200px]">
-        <NuxtImg
-          :src="item.image"
-          width="262"
-          height="200"
-          class="rounded-2xl object-cover w-full h-full"
-        />
-      </div>
-
-      <div class="mx-auto w-[262px] flex flex-col gap-2 flex-1">
-        <div class="flex flex-row justify-between flex-1">
-          <div class="flex flex-col gap-3">
-            <p>{{ item.name }}</p>
-
-            <ul class="inline-block">
-              <li class="flex items-center gap-2">
-                <IconsAlive /> {{ item.status }}
-              </li>
-              <li class="flex items-center gap-2">
-                <IconsHuman /> {{ item.species }}
-              </li>
-              <li class="flex items-center gap-2">
-                <IconsEarth /> {{ item.origin.name }}
-              </li>
-            </ul>
-          </div>
-
-          <div class="mt-2">
-            <IconsHeartFull v-if="item.status === 'Alive'" />
-            <IconsHeartVoid v-else />
-          </div>
-        </div>
-
-        <NuxtLink :to="item.url">
-          <div
-            class="flex gap-1 place-self-end px-3 py-2 rounded-full bg-[#11B0C8] text-sm items-center mb-4"
-          >
-            <IconsReadmore />
-            <span>Saiba Mais</span>
-          </div>
-        </NuxtLink>
-      </div>
+      <Card
+        :image="item.image"
+        :name="item.name"
+        :status="item.status"
+        :species="item.species"
+        :planet="item.planet"
+        :url="item.url"
+      />
     </div>
   </div>
 </div>
---
+++ components/Card.vue
@@ -0,0 +1,77 @@
+<template>
+      <div class="mx-auto mt-4 border border-[#000000] rounded-2xl w-[262px] h-[200px]">
+        <NuxtImg
+          :src="image"
+          width="262"
+          height="200"
+          class="rounded-2xl object-cover w-full h-full"
+        />
+      </div>
+
+      <div class="mx-auto w-[262px] flex flex-col gap-2 flex-1">
+        <div class="flex flex-row justify-between flex-1">
+          <div class="flex flex-col gap-3">
+            <p>{{ name }}</p>
+
+            <ul class="inline-block">
+              <li class="flex items-center gap-2">
+                <IconsAlive /> {{ status }}
+              </li>
+              <li class="flex items-center gap-2">
+                <IconsHuman /> {{ species }}
+              </li>
+              <li class="flex items-center gap-2">
+                <IconsEarth /> {{ planet }}
+              </li>
+            </ul>
+          </div>
+
+          <div class="mt-2">
+            <IconsHeartFull v-if="status === 'Alive'" />
+            <IconsHeartVoid v-else />
+          </div>
+        </div>
+
+        <NuxtLink :to="url">
+          <div
+            class="flex gap-1 place-self-end px-3 py-2 rounded-full bg-[#11B0C8] text-sm items-center mb-4"
+          >
+            <IconsReadmore />
+            <span>Saiba Mais</span>
+          </div>
+        </NuxtLink>
+      </div>
+</template>
+<script lang="ts" setup>
+const props = defineProps({
+  image: {
+    type: String,
+    required: true
+  },
+
+  name: {
+    type: String,
+    required: true
+  },
+
+  status: {
+    type: String,
+    required: true
+  },
+
+  species: {
+    type: String,
+    required: true
+  },
+
+  planet: {
+    type: String,
+    required: true
+  },
+
+  url: {
+    type:String,
+    required: true
+  }
+})
+</script>
