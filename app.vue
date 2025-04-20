242f615894129280e942576b6e010e7d760ddf9e
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2025-04-20 10:16:32 -0300

    inicia card

--- app.vue
+++ app.vue
@@ -1,7 +1,33 @@
 <template>
   <div>
-    <div class="">
-      <h1> {{ data.results[0].name }} </h1>
+    <div class="text-white">
+
+      <div class="flex flex-col bg-[#313234] card w-[294px] h-[400px] mx-auto">
+        <div class="mx-auto flex flex-col gap-2">
+          <div class="mx-auto mt-4 border border-[#000000] rounded-2xl w-[262px] h-[200px]">
+            <NuxtImg
+              :src="data.results[0].image"
+              :key="data.results[0].id"
+              width="262"
+              height="200"
+              class="rounded-2xl object-cover w-full h-full"
+            />
+          </div>
+
+          <div class="text-white">
+            <p> {{ data.results[0].name }} </p>
+          </div>
+        </div>
+      </div>
+
+
+
+
+      <p> {{ data.results[0].created }} </p>
+      <p> {{ data.results[0].episode }} </p>
+      <p> {{ data.results[0].id }} </p>
+      <h1> {{ data.results[0].status }} </h1>
+      <h1> {{ data.results[0].species }} </h1>
     </div>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
+        },
+      ],
+    },
+  },
 })
