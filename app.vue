d6adbba9f48d16e3d8124c9a60302ad0f1986a7e
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2025-04-20 17:17:25 -0300

    incia header

--- app.vue
+++ app.vue
@@ -1,4 +1,26 @@
 <template>
+<div class="w-full h-[580px] bg-black text-white">
+  <div class="mx-auto container max-w-[1250px]">
+    <div class="flex justify-between py-14">
+      <NuxtImg
+        src="img/logo.png"
+        height="64"
+        width="219"
+      />
+
+        <div
+          class="flex gap-2 place-self-end px-3 py-2 rounded-full bg-[#11B0C8] text-sm items-center mb-4"
+        >
+          <IconsHeartVoidWhite />
+          <span>Lista de favoritos</span>
+        </div>
+    </div>
+  </div>
+</div>
+
+
+
+<!--  -->
 <div class="bg-gray-900">
   <div class="flex flex-wrap gap-4 justify-center max-w-[1300px] mx-auto">
     <div
@@ -21,6 +43,8 @@
 
 
 <script setup lang="ts">
+import { IconsHeartVoid } from '#components';
+
 const { data, status, error, refresh, clear } = await useFetch("https://rickandmortyapi.com/api/character");
 
 console.log(data.value);
