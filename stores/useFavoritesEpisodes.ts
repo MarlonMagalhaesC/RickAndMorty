ee958d648dfe889db631a03cb7e951425237ee02
Marlon Magalhaes <marlonmagalhaescar@hotmail.com>
2025-05-03 14:15:59 -0300

    cria store UseFavoritesEpisodes

---
+++ stores/useFavoritesEpisodes.ts
@@ -0,0 +1,22 @@
+import { defineStore } from 'pinia'
+import type { Episode } from '~/types/Episode'
+
+export const useFavoritesEpisodes = defineStore('favorites', () => {
+
+const favorites = ref<(Episode[])>([])
+
+const addFavorite = (ep: Episode) => {
+  favorites.value.push(ep)
+  console.log(ep)
+}
+
+const removeFavorite = (id: number) => {
+  favorites.value = favorites.value.filter(e => e.id !== id)
+}
+
+return{
+  favorites,
+  addFavorite,
+  removeFavorite
+}
+})
